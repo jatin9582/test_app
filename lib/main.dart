33e81eacd80ef0ce385c7a4691c4da5b488c46e0
Jatin <jatin.rawat@enrichai.com>
2023-01-11 19:13:44 +0530

    added app center

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_appcenter_bundle/flutter_appcenter_bundle.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await AppCenter.startAsync(
+    appSecretAndroid: '326287de-3d19-4230-bf7f-1945aca81448',
+    appSecretIOS: '',
+    enableDistribute: false,
+  );
   runApp(const MyApp());
 }
 
@@ -61,6 +68,14 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    AppCenter.trackEventAsync('_MyAppState.initState');
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -79,20 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
